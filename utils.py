31541eb82aa69512e6f4a94c632a0b710ccc9702
Reza Tavasoli <jfk998@gmail.com>
2019-08-01 18:07:39 +0430

    Add boxes, units and peers variables

--- utils.py
+++ utils.py
@@ -4,3 +4,12 @@ cols = '123456789'
 
 def cross(a, b):
     return [s + t for s in a for t in b]
+
+
+boxes = cross(rows, cols)
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')]
+unit_list = row_units + column_units + square_units
+units = dict((s, [u for u in unit_list if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s], [])) - {s}) for s in boxes)
