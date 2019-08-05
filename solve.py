d6ee1d31272ef21d6e5470cec5334127fcab9e23
Reza Tavasoli <jfk998@gmail.com>
2019-08-05 22:07:08 +0430

    Fix typo in input text

--- solve.py
+++ solve.py
@@ -3,7 +3,7 @@ from utils import *
 
 def solve():
     sudoku_input = input('''Enter sudoku's numbers like example (use a '.' as a placeholder for an empty box)
-(Ex. '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3)
+(Ex. 2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3)
 ''')
     display(search(grid_values(sudoku_input)))
 
